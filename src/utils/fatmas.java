5c97358daeafc11c720fea690b4ea786f0969503
faydogdu <ftmaydgd@gmail.com>
2023-01-04 20:47:36 -0700

    fatma created

--- src/utils/fatmas.java
+++ src/utils/fatmas.java
@@ -1,4 +1,9 @@
 package utils;
 
 public class fatmas {
+    public static void main(String[] args) {
+
+        System.out.println("fatmaaaaa");
+        System.out.println("javaaaaaaaaaaaa");
+    }
 }
