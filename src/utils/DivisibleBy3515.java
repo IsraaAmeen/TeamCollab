8f25d65e7153c1691d6523f6d304ea6df6090ebc
aima-z <aimkan86@gmail.com>
2023-01-05 13:42:16 -0600

    Java coding tasks 2

---
+++ src/utils/DivisibleBy3515.java
@@ -0,0 +1,27 @@
+package utils;
+
+public class DivisibleBy3515 {
+
+    public static void main(String[] args) {
+DivisibleBy();
+    }
+
+    public static void DivisibleBy(){
+        String divBy3 ="Divisible By 15: ";
+        String divBy5="Divisible By 5: ";
+        String divBy15="Divisible By 15: ";
+
+        for(int i=1; i<=100; i++){
+            if(i%3==0 && i%5==0 && i%15==0){
+                divBy15+=(" "+i);
+            }else if(i%3==0 && !(i%15==0)){
+                divBy3+=(" "+i);
+            }else if(i%5==0 && !(i%15==0)){
+                divBy5+=(" "+i);
+            }
+        }
+        System.out.println(divBy15);
+        System.out.println(divBy5);
+        System.out.println(divBy3);
+    }
+}
