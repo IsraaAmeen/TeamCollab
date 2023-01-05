cae5c78a6f18cfcf7e6c737ddcdaf1c6955a0bf1
aima-z <aimkan86@gmail.com>
2023-01-04 21:35:51 -0600

    create aima class

--- src/utils/aima.java
+++ src/utils/aima.java
@@ -5,5 +5,6 @@ public class aima {
     public static void main(String[] args) {
         System.out.println("aima");
         System.out.println("amamamamama");
+        System.out.println("conflict created");
     }
 }
