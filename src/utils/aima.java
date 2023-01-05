1dc3524cfee621f679abd45e354b08cd8797a14b
IsraaAmeen <israa.ameen@yahoo.com>
2023-01-04 21:39:45 -0600

    israa created class

--- src/utils/aima.java
+++ src/utils/aima.java
@@ -5,5 +5,8 @@ public class aima {
     public static void main(String[] args) {
         System.out.println("aima");
         System.out.println("amamamamama");
+        System.out.println("israa creating conflict");
+
+
     }
 }
